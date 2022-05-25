391d03e4a7c8b604f086e5d34f7e9cd442bbbf33
zj-landeed <zj@golory.com>
2022-05-25 13:22:02 -0700

    refactor queries to its own file

--- backend/lib/graphql/schema.py
+++ backend/lib/graphql/schema.py
@@ -1,23 +1,6 @@
 import graphene
-from graphene import ObjectType, String, Schema
-from graphene_sqlalchemy import SQLAlchemyObjectType
-from models.vendor_model import VendorModel
-
-class Vendor(SQLAlchemyObjectType):
-  class Meta:
-    model = VendorModel
-
-class Query(ObjectType):
-  vendors = graphene.List(Vendor)
-  vendor = graphene.Field(Vendor, id=graphene.Int())
-  
-  def resolve_vendors(root, info):
-    query = Vendor.get_query(info)
-    return query.all()
-
-  def resolve_vendor(root, info, id):
-    query = Vendor.get_query(info)
-    return query.filter(VendorModel.id == id).first()
+from graphene import Schema
+from .query import Query
     
 
 schema = Schema(query=Query)
